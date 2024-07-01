b09b389d41d70b82b615ae85d02fbb36d35cd29f
Artem <pakshaartem@gmail.com>
2024-07-01 14:28:59 +0300

    some fixes

--- app/math/translator.js
+++ app/math/translator.js
@@ -6,52 +6,51 @@ export const translate = (systemFrom, systemTo, value) => {
     const hexReg = /^[0-9A-Fa-f]+$/;
 
     if (systemFrom === 'BIN') {
-        console.log('123', (binReg.test(value)))
         if (!binReg.test(value)) return 'Invalid bin value'
         switch (systemTo) {
-            case 'BIN':
+            case 'BIN ':
                 return numericValue
-            case 'OCT':
+            case 'OCT ':
                 return parseInt(numericValue, 2).toString(8);
-            case 'DEC':
+            case 'DEC ':
                 return parseInt(numericValue, 2);
-            case 'HEX':
+            case 'HEX ':
                 return parseInt(numericValue, 2).toString(16);
         }
     } else if (systemFrom === 'OCT') {
         if (!octReg.test(value)) return 'Invalid oct value'
         switch (systemTo) {
-            case 'BIN':
+            case 'BIN ':
                 return parseInt(numericValue, 8).toString(2);
-            case 'OCT':
+            case 'OCT ':
                 return numericValue
-            case 'DEC':
+            case 'DEC ':
                 return parseInt(numericValue, 8);
-            case 'HEX':
+            case 'HEX ':
                 return parseInt(numericValue, 8).toString(16);
         }
     } else if (systemFrom == 'DEC') {
         if (!decReg.test(value)) return 'Invalid dec value'
         switch (systemTo) {
-            case 'BIN':
+            case 'BIN ':
                 return numericValue.toString(2);
-            case 'OCT':
+            case 'OCT ':
                 return numericValue.toString(8);
-            case 'DEC':
+            case 'DEC ':
                 return numericValue
-            case 'HEX':
+            case 'HEX ':
                 return numericValue.toString(16);
         }
     } else if (systemFrom === 'HEX') {
         if (!hexReg.test(value)) return 'Invalid hex value'
         switch (systemTo) {
-            case 'BIN':
+            case 'BIN ':
                 return parseInt(numericValue, 16).toString(2);
-            case 'OCT':
+            case 'OCT ':
                 return parseInt(numericValue, 16).toString(8);
-            case 'DEC':
+            case 'DEC ':
                 return parseInt(numericValue, 16);
-            case 'HEX':
+            case 'HEX ':
                 return numericValue;
         }
     }
